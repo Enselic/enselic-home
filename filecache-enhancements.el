c12122252e4b44b7e6efba20c82cacbf505a0d93
Martin Nordholts <enselic@gmail.com>
2008-04-19 08:53:16 +0200

    Format and add file-cache-number-of-items helper function

--- filecache-enhancements.el
+++ filecache-enhancements.el
@@ -4,7 +4,6 @@
 ;;
 
 (defvar file-cache-files-matching-map nil)
-
 (if file-cache-files-matching-map
     ()
   (setq file-cache-files-matching-map (make-sparse-keymap))
@@ -12,54 +11,6 @@
   (define-key file-cache-files-matching-map (kbd "n")   'next-line)
   (define-key file-cache-files-matching-map (kbd "p")   'previous-line))
 
-
-
-(defun file-cache-save-cache-to-file (file)
-  "Save contents of `file-cache-alist' to FILE.
-For later retrieval using `file-cache-read-cache-from-file'"
-  (interactive "FFile: ")
-  (with-temp-file (expand-file-name file)
-    (prin1 file-cache-alist (current-buffer))))
-
-(defun file-cache-read-cache-from-file (file)
-  "Clear `file-cache-alist' and read cache from FILE.
-The file cache can be saved to a file using
-`file-cache-save-cache-to-file'."
-  (interactive "fFile: ")
-  (file-cache-clear-cache)
-  (let ((buf (find-file-noselect file)))
-    (setq file-cache-alist (read buf))
-    (kill-buffer buf)))
-
-(defun file-cache-ido-find-file (file)
-  "Using ido, interactively open file from file cache'.
-First select a file, matched using ido-switch-buffer against the contents
-in `file-cache-alist'. If the file exist in more than one
-directory, select directory. Lastly the file is opened."
-  (interactive (list (file-cache-ido-read "File: "
-                                          (mapcar
-                                           (lambda (x)
-                                             (car x))
-                                           file-cache-alist))))
-  (let* ((record (assoc file file-cache-alist)))
-    (find-file
-     (expand-file-name
-      file
-      (if (= (length record) 2)
-          (car (cdr record))
-        (file-cache-ido-read
-         (format "Find %s in dir: " file) (cdr record)))))))
-
-(defun file-cache-ido-read (prompt choices)
-  (let ((ido-make-buffer-list-hook
-	 (lambda ()
-	   (setq ido-temp-list choices))))
-    (ido-read-buffer prompt)))
-
-(defun file-cache-add-this-file ()
-  (and buffer-file-name
-       (file-exists-p buffer-file-name)
-       (file-cache-add-file buffer-file-name)))
 
 
 (defun file-cache-files-matching-with-mode ()
@@ -87,6 +38,63 @@ directory, select directory. Lastly the file is opened."
   (use-local-map file-cache-files-matching-map)
   (setq mode-name "FCFMM")
   (setq major-mode 'file-cache-files-matching-mode))
+
+
+(defun file-cache-save-cache-to-file (file)
+  "Save contents of `file-cache-alist' to FILE.
+For later retrieval using `file-cache-read-cache-from-file'"
+  (interactive "FFile: ")
+  (with-temp-file (expand-file-name file)
+    (prin1 file-cache-alist (current-buffer))))
+
+
+(defun file-cache-read-cache-from-file (file)
+  "Clear `file-cache-alist' and read cache from FILE.
+The file cache can be saved to a file using
+`file-cache-save-cache-to-file'."
+  (interactive "fFile: ")
+  (file-cache-clear-cache)
+  (let ((buf (find-file-noselect file)))
+    (setq file-cache-alist (read buf))
+    (kill-buffer buf)))
+
+
+(defun file-cache-ido-find-file (file)
+  "Using ido, interactively open file from file cache'.
+First select a file, matched using ido-switch-buffer against the contents
+in `file-cache-alist'. If the file exist in more than one
+directory, select directory. Lastly the file is opened."
+  (interactive (list (file-cache-ido-read "File: "
+                                          (mapcar
+                                           (lambda (x)
+                                             (car x))
+                                           file-cache-alist))))
+  (let* ((record (assoc file file-cache-alist)))
+    (find-file
+     (expand-file-name
+      file
+      (if (= (length record) 2)
+          (car (cdr record))
+        (file-cache-ido-read
+         (format "Find %s in dir: " file) (cdr record)))))))
+
+
+(defun file-cache-ido-read (prompt choices)
+  (let ((ido-make-buffer-list-hook
+	 (lambda ()
+	   (setq ido-temp-list choices))))
+    (ido-read-buffer prompt)))
+
+
+(defun file-cache-add-this-file ()
+  (and buffer-file-name
+       (file-exists-p buffer-file-name)
+       (file-cache-add-file buffer-file-name)))
+
+
+(defun file-cache-number-of-items ()
+  (length file-cache-alist))
+
 
 
 (provide 'filecache-enhancements)
