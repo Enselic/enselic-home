45c19595b01738c7490ee75bcdba7d21af2c5640
Martin Nordholts <enselic@gmail.com>
2008-04-22 18:14:18 +0200

    Added *NIX and Windows scripts for creating programming projects.

---
+++ create-project.bat
@@ -0,0 +1,2 @@
+@echo off
+"c:\Program Files\emacs-22.2\bin\emacs.exe" -batch -l ~/.emacs -f programming-project-batch-create %*
---
+++ create-project.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+emacs22 -batch -l ~/.emacs -f programming-project-batch-create $@
+
