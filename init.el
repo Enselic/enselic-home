37629af49f77b28ccd0b4ae838e98300c44289d2
Martin Nordholts <enselic@gmail.com>
2008-09-03 18:42:03 +0200

    Added 4sp-linux C-style.

--- init.el
+++ init.el
@@ -53,6 +53,7 @@
 (setq-default cursor-type '(bar . 2))
 (c-add-style "2sp-linux" '("linux" (c-basic-offset . 2)))
 (c-add-style "3sp-linux" '("linux" (c-basic-offset . 3)))
+(c-add-style "4sp-linux" '("linux" (c-basic-offset . 4)))
 (fset 'yes-or-no-p 'y-or-n-p)
 
 
