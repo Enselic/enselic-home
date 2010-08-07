5e64ca7d5a5171e4594cf6f25e2527f270bd6ed7
Martin Nordholts <martinn@src.gnome.org>
2010-08-07 11:01:15 +0200

    Rename bashrc to bashrc.sh and add croot()

--- bashrc.sh
+++ bashrc.sh
@@ -2,3 +2,10 @@ export PATH="/home/martin/source/pastebinit-1.1:/home/martin/bin:/home/martin/en
 export EDITOR="gedit"
 export PS1='\[\033[1m\]\u@\h:\w\$ \[\033[0m\]'
 alias cd..='cd ..'
+
+function croot()
+{
+    while [ ! -d ".git" ]; do
+        cd ..
+    done
+}
