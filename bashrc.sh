0fd85ac580cbbb22d40e265cde1f6c150106df1d
Martin Nordholts <martinn@src.gnome.org>
2011-04-14 10:20:36 +0200

    add installtarball and fix paths

--- bashrc.sh
+++ bashrc.sh
@@ -1,5 +1,4 @@
-export PYTHONPATH="/home/martin/prefixes/buildbot-master/lib/python2.6/site-packages"
-export PATH="/home/martin/prefixes/buildbot-master/bin:/home/martin/source/gimp-buildbot-utils/shellscripts:/home/martin/source/pastebinit-1.1:/home/martin/bin:/home/martin/enselic-home/bin:$PATH"
+export PATH="$HOME/bin:$HOME/enselic-home/bin:$PATH"
 export EDITOR="gedit"
 export PS1='\[\033[1m\]\u@\h:\w\$ \[\033[0m\]'
 alias cd..='cd ..'
@@ -7,14 +6,14 @@ export HISTSIZE=10000
 
 # Change to first ancestor dir with a .git subfolder, while avoiding
 # to mess up "cd -"
-function groot()
+groot()
 {
     previous_dir=`cd -`
     original_dir=`pwd`
     while [ ! -d ".git" ] && [ ! `pwd` = "/" ]; do
         cd ..
     done
- 
+
     if [ ! -d ".git" ]; then
         echo "No .git found in current or ancestor dirs"
         cd "$previous_dir"
@@ -25,4 +24,26 @@ function groot()
         cd "$original_dir"
         cd "$new_dir"
     fi
+}
+
+installtarball()
+{
+    if [ $# -lt 2 ]; then
+        echo "Usage: installtarball PREFIX URL"
+        return;
+    fi
+
+    prefix=$1
+    url=$2
+
+    tarballname=${url##*/}
+    dirname=${tarballname%.tar.*}
+
+    wget ${url}
+    tar -xvf ${tarballname}
+    cd ${dirname}
+    ./configure --prefix=${prefix}
+    make -j5
+    make install
+    cd ..
 }
