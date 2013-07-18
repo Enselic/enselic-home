35fe39c34c63a1438d1241de19fae6169102174f
Martin Nordholts <martin.nordholts@sonymobile.com>
2013-07-18 09:54:22 +0200

    Add Groovy Mode
    From http://groovy.codehaus.org/Emacs+Groovy+Mode

--- elisp/init.el
+++ elisp/init.el
@@ -71,6 +71,22 @@
 (set-background-color "white")
 
 
+;; From http://groovy.codehaus.org/Emacs+Groovy+Mode
+
+;;; use groovy-mode when file ends in .groovy or has #!/bin/groovy at start
+(autoload 'groovy-mode "groovy-mode" "Major mode for editing Groovy code." t)
+(add-to-list 'auto-mode-alist '("\.groovy$" . groovy-mode))
+(add-to-list 'interpreter-mode-alist '("groovy" . groovy-mode))
+(add-to-list 'auto-mode-alist '("\.gradle$" . groovy-mode))
+
+;;; make Groovy mode electric by default.
+(add-hook 'groovy-mode-hook
+          '(lambda ()
+             (require 'groovy-electric)
+             (groovy-electric-mode)))
+
+
+
 (add-hook 'after-init-hook  'safe-load-abbrevs)
 (add-hook 'after-init-hook  'session-initialize)
 (add-hook 'after-init-hook  'grep-compute-defaults)
