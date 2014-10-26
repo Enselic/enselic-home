c85cf97774924630740004e680957bc7a0e6c915
Martin Nordholts <enselic@gmail.com>
2014-10-26 19:31:38 +0100

    List dirs with only duplicates

--- bin/list-duplicate-sha1.py
+++ bin/list-duplicate-sha1.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python
 
+# Generate a file with "SHA1 dir/filename.ext" pairs on each line with this command (OS X):
+# time find . -iname '*.jpg' -exec shasum {} + > martin/Desktop/really-all-jpgs-shasum.txt
+
 import sys
 import fileinput
 
@@ -8,6 +11,7 @@ class FileNode:
     def __init__(self, name):
         self.name = name
         self.children = {};
+        self.n_children_with_duplicates = 0
         self.full_name = None
         self.duplicates = None
 
@@ -27,15 +31,21 @@ for line in fileinput.input():
 
     segments = filename.split("/")
     current_node = root
+    current_full_name = ""
     for segment in segments:
+        current_full_name += segment + "/"
         if not segment in current_node.children:
-            current_node.children[segment] = FileNode(segment)
+            new_node = FileNode(segment)
+            new_node.full_name = current_full_name
+            new_node.parent = current_node
+            current_node.children[segment] = new_node
         current_node = current_node.children[segment]
 
     current_node.full_name = filename
     current_node.duplicates = duplicates_list
 
 
+dirs_with_duplicates = []
 def print_node(node, indent=""):
     # Print name
     sys.stdout.write("\n")
@@ -45,10 +55,21 @@ def print_node(node, indent=""):
     for child in node.children:
         print_node(node.children[child], indent + "  ")
 
+    if len(node.children) > 0 and node.n_children_with_duplicates == len(node.children):
+        dirs_with_duplicates.append(node.full_name)
+        print("ADDING:", node.full_name)
+
     if node.duplicates is not None:
         duplicates_without_self = list(node.duplicates)
         duplicates_without_self.remove(node.full_name)
         if len(duplicates_without_self) > 0:
             sys.stdout.write("  [" + ' '.join(duplicates_without_self) + "]")
+            if node.parent is not None:
+                node.parent.n_children_with_duplicates += 1
 
 print_node(root)
+
+print("Dirs with only duplicates:")
+for d in dirs_with_duplicates:
+    print d
+
