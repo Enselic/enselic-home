c9b59912ab56665abf42152ad30b1cc9b1e92d6a
Martin Nordholts <enselic@gmail.com>
2016-09-12 06:37:38 +0200

    Remove create-project

--- bin/create-project.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-"c:\Program Files\emacs-22.2\bin\emacs.exe" -batch -l ~/.emacs -f programming-project-batch-create %*
--- bin/create-project.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-project_name="$1"
-if [ -z "$project_name" ]; then
-    project_name=$(basename `pwd`)
-fi
-
-emacs -batch -l ~/.emacs -f programming-project-batch-create "$project_name"
-
