bfff1363d79d22c53c356ffebc56a4705695c4ed
Martin Nordholts <enselic@gmail.com>
2017-02-14 20:11:03 +0100

    Add gerrit_cherry_pick

--- shell/common.sh
+++ shell/common.sh
@@ -3,6 +3,7 @@ alias cd..='cd ..'
 alias check="git diff --check HEAD^..HEAD"
 alias now="date +%Y-%m-%d_%H%M%S"
 alias gitkk="gitk --all"
+alias gcp=gerrit_cherry_pick
 
 # PATH
 export PATH="$HOME/enselic-home/bin:$PATH"
@@ -40,4 +41,23 @@ groot()
         cd "$original_dir"
         cd "$new_dir"
     fi
+}
+
+gerrit_cherry_pick() {
+    if [ -z "$1" ]; then
+        echo "Usage: gerrit_cherry_pick nnnnnnn [pp]"
+        return 1
+    fi
+    # http://foo.bar.com/r/123456/ -> 123456
+    change="$(echo $1 | sed 's/[^0-9]*\([0-9]\+\)[^0-9]*/\1/g')"
+    if [ -z "$2"]; then
+        patchSet="latest"
+    else
+        patchSet="$2"
+    fi
+
+    git fetch origin "refs/changes/${change: -2}/${change}/${patchSet}" && \
+    git cherry-pick --no-commit FETCH_HEAD && \
+    ! git diff --exit-code --cached --quiet && \
+    git commit --no-verify --no-edit
 }
